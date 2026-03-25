dfff759750b864bd254aa1658e9bb6cab9e90056
saadazghour <saad.azghour@gmail.com>
2026-03-25 15:10:11 +0100

    feat: Add O(1) constant time complexity example
    Adds a new function addItems that demonstrates constant time O(1) complexity.
    where the operation takes the same time regardless of input size.

---
+++ 00-Misc/addItems.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int addItems(int n){
+  return n + n;
+  // What if we had n + n + n ----> Here two operations -- O(2) ---> then wer's simplify this --> O(1)
+}
+
+// O(1)
+int main(){
+  int sum;
+
+  sum = addItems(10);
+
+  cout << sum;
+}
