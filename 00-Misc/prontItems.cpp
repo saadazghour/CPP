7f7c892b77d413215bd909982b6d04b53546594e
saadazghour <saad.azghour@gmail.com>
2026-03-25 14:33:37 +0100

    docs: explain time complexity with Big O notation & drop Big O: Drop Non-Dominants concept

--- 00-Misc/prontItems.cpp
+++ 00-Misc/prontItems.cpp
@@ -3,14 +3,23 @@
 using namespace std;
 
 void printItems(int n){
+  // O(n^2)
   for (int i = 0; i < n; i++){
     for (int j = 0; j < n; j++){
       cout << i << j << endl;
     }
   }
+
+  // O(n)
+  for (int k = 0; k < n; k++){
+   cout << k << endl;
+  }
+
 }
 
-// O(n^2) - n * n = n^2 - O(n^2)
+// The total numbers of operations is O(n^2) + O(n) - O(n^2 + n)
+
+// Big O: Drop Non-Dominants, Then we simplify this by just keeping ---> O(n^2)
 int main(){
   printItems(10);
 }
