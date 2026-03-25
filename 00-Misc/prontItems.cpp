98fa436aa6c3264beaacff93f335701a6e18fc91
saadazghour <saad.azghour@gmail.com>
2026-03-25 13:46:32 +0100

    feat: Add second print loop with O(n) complexity - Drop Constants - Simplifications

--- 00-Misc/prontItems.cpp
+++ 00-Misc/prontItems.cpp
@@ -6,8 +6,14 @@ void printItems(int n){
   for (int i = 0; i < n; i++){
     cout << i << endl;
   }
+
+  for (int j = 0; j < n; j++)
+  {
+    cout << j << endl;
+  }
 }
 
+// Drop Constants O(2n) - n + n = 2n - O(2n) - O(n)
 
 // O(n)
 int main(){
